38fff88f18dfd3b70e14a6e8003b8c11535c6385
Joseph Muruguru <Jmuruguru@wireless1x-168-122-14-116.bu.edu>
2015-03-12 12:15:39 -0400

    added user authentification

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,18 +1,32 @@
-SC.initialize({
-	client_id: 'd1406a91a61f2077f85d9e0687a6ad75',
-	redirect_uri: "https://soundcloud.com",
-});
 var play = document.getElementById('play');
 var pause = document.getElementById('pause');
 var username = document.getElementById('username');
+var player = document.getElementById('player')
 
-SC.stream('/tracks/293', function(sound){
-	play.addEventListener('click', function(e){
-		sound.start();
-	});
-	pause.addEventListener('click', function(e){
-		sound.pause();
-	});
+SC.initialize({
+	client_id: 'd1406a91a61f2077f85d9e0687a6ad75',
+	redirect_uri: 'http://0.0.0.0:8080/redirect.html'
 });
+
+// SC.stream('/tracks/293', function(sound){
+// 	play.addEventListener('click', function(e){
+// 		sound.start();
+// 	});
+// 	pause.addEventListener('click', function(e){
+// 		sound.pause();
+// 	});
+// });
+
+function mySounds(){
+	SC.connect(function(){
+		SC.get('/me', function(me){
+			alert('Hello, ' + me.username);
+		});
+	});
+}
+
+mySounds();
+
+
 
 
