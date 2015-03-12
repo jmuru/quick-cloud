e8dfb88042fb5da5ec562518ac7b39bbc6de1c10
Joseph Muruguru <Jmuruguru@wireless1x-168-122-14-116.bu.edu>
2015-03-12 17:51:23 -0400

    added ways to get user_id and duration, can now pull profiles from soundcloud and play their posted music. current changes: find a way to randomly loop through all of the tracks

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -2,10 +2,13 @@ var play = document.getElementById('play');
 var pause = document.getElementById('pause');
 var username = document.getElementById('username');
 var player = document.getElementById('player')
+var ul = document.getElementById('track-listing');
+var node = document.createElement('LI');
+
 
 SC.initialize({
-	client_id: 'd1406a91a61f2077f85d9e0687a6ad75',
-	redirect_uri: 'http://0.0.0.0:8080/redirect.html'
+    client_id: 'd1406a91a61f2077f85d9e0687a6ad75',
+    redirect_uri: 'http://0.0.0.0:8080/redirect.html'
 });
 
 // SC.stream('/tracks/293', function(sound){
@@ -17,16 +20,57 @@ SC.initialize({
 // 	});
 // });
 
-function mySounds(){
-	SC.connect(function(){
-		SC.get('/me', function(me){
-			alert('Hello, ' + me.username);
-		});
-	});
+// function mySounds(){
+// 	SC.connect(function(){
+// 		SC.get('/me', function(me){
+// 			alert('Hello, ' + me.username);
+// 		});
+// 	});
+// }
+
+// mySounds();
+
+function playMusic(track) {
+    SC.stream('/tracks/' + track.id, function(track) {
+        play.addEventListener('click', function(e) {
+            play.style.visibility = 'hidden';
+            pause.style.visibility = 'initial';
+            track.play();
+
+        });
+        pause.addEventListener('click', function(e) {
+            play.style.visibility = 'initial';
+            pause.style.visibility = 'hidden';
+            track.pause();
+        });
+    });
 }
 
-mySounds();
+// helper function that return length of track in miliseconds
+function getTrackDuration(track) {
+    //console.log(track.duration);
+    return track.duration;
+}
 
+function selectedArtist() {
+    SC.get('/tracks', {
+        user_id: 483960,
+        limit: 20
+    }, function(tracks) {
+        console.log(tracks);
+        var random = Math.floor(Math.random() * tracks.length);
+        var duration = getTrackDuration(tracks[random]);
+        console.log(duration);
+        playMusic(tracks[random]);
+    });
+}
 
-
-
+// helper function to get user id of specific artist on soundcloud
+//takes in a string version of the url appends it to the resolve api reference
+// function getId(url) {
+//     SC.get('/resolve/?url=' + url, function(result) {
+//         console.log(result);
+//     });
+// }
+//getId('https://soundcloud.com/Soulection');
+selectedArtist();
