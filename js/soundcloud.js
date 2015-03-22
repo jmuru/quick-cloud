3517bfc4d804e8192a94c3f95d2fc1b5fe4ecacb
Joseph Muruguru <Jmuruguru@wireless1x-168-122-1-103.bu.edu>
2015-03-22 01:03:36 -0400

    hirokis suggestions/ code improvement

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -1,9 +1,3 @@
-
-var trackName = document.getElementById('track-name');
-var artist = document.getElementById('artist');
-var artwork = document.getElementById('artwork');
-
-
 // SC.stream('/tracks/293', function(sound){
 //  play.addEventListener('click', function(e){
 //      sound.start();
@@ -33,81 +27,89 @@ var artwork = document.getElementById('artwork');
 // }
 //getId('https://soundcloud.com/Soulection');
 
+(function(){
+    'use strict';
 
-SC.initialize({
-    client_id: 'd1406a91a61f2077f85d9e0687a6ad75',
-    redirect_uri: 'http://0.0.0.0:8080/redirect.html'
-});
 
-//return length of track in miliseconds
-function getTrackDuration(track) {
-    //console.log(track.duration);
-    return track.duration;
-}
+    var $trackName = document.getElementById('track-name');
+    var $artist = document.getElementById('artist');
+    var $artwork = document.getElementById('artwork');
+    var $play = document.getElementById('play');
+    var $pause = document.getElementById('pause');
+    var status = 0, track;
 
-function getTrackName(track){
-    return track.title;
-}
 
-function getArtistName(track){
-    return track.user.username;
-}
 
-function getBackground(track){
-    return track.artwork_url;
-}
 
-function playMusic(track) {
-    var play = document.getElementById('play');
-    var pause = document.getElementById('pause');
+    SC.initialize({
+        client_id: 'd1406a91a61f2077f85d9e0687a6ad75',
+        redirect_uri: 'http://0.0.0.0:8080/redirect.html'
+    });
 
-    SC.stream('/tracks/' + track.id, function(track) {
-        play.addEventListener('click', function(e) {
-            play.style.visibility = 'hidden';
-            pause.style.visibility = 'initial';
+    function play(){
+        $play.style.visibility = 'hidden';
+        $pause.style.visibility = 'initial';
+
+        console.log("Play triggered");
+        if( track ){
             track.play();
-
-        });
-        pause.addEventListener('click', function(e) {
-            play.style.visibility = 'initial';
-            pause.style.visibility = 'hidden';
-            track.pause();
-        });
-    });
-}
-
-function dwnld(track){
-    if(track.downloadable === true){
-        var link = document.createElement('a');
-        // creates text in between the opening and closing html tags
-        var linkText = document.createTextNode('Download Now');
-        link.appendChild(linkText);
-        link.id = 'download';
-        link.href = track.download_url;
-        return document.body.appendChild(link);
-    }else{
-        return "";
+        }
+        status = 1;
     }
-}
+    function pause() {
+        $play.style.visibility = 'initial';
+        $pause.style.visibility = 'hidden';
+        track.pause();
+        status = 0;
+    }
 
-function selectedArtist() {
-    SC.get('/tracks', {
-        user_id: 483960,
-        limit: 30
-    }, function(tracks) {
-        //console.log(tracks);
-        var random = Math.floor(Math.random() * tracks.length);
-        var duration = getTrackDuration(tracks[random]);
-        //console.log(duration);
-        console.log(tracks[random]);
-        playMusic(tracks[random]);
-        trackName.textContent = getTrackName(tracks[random]);
-        artist.textContent = 'Artist: ' + getArtistName(tracks[random]);
-        console.log(getBackground(tracks[random]));
-        artwork.src = getBackground(tracks[random]);
-        dwnld(tracks[random]);
-    });
-}
+    $play.addEventListener('click', play);
 
+    $pause.addEventListener('click', pause);
 
-selectedArtist();
+    function startStream(_track, cb) {
+        SC.stream('/tracks/' + _track.id, cb);
+    }
+
+    function dwnld(track) {
+        if (track.downloadable === true) {
+            var link = document.createElement('a');
+            // creates text in between the opening and closing html tags
+            var linkText = document.createTextNode('Download Now');
+            link.appendChild(linkText);
+            link.id = 'download';
+            link.href = track.download_url;
+            return document.body.appendChild(link);
+        } else {
+            return "";
+        }
+    }
+
+    function selectedArtist() {
+        SC.get('/tracks', {
+            user_id: 483960,
+            limit: 30
+        }, function(tracks) {
+            //console.log(tracks);
+            var randomIdx = ~~(Math.random() * tracks.length);
+            var selTrack = tracks[randomIdx];
+
+            // var duration = getTrackDuration(track);
+
+            startStream(selTrack, function(_track){
+
+                track = _track;
+                if( status == 1 ){
+                    play();
+                }
+            });
+            $trackName.textContent = selTrack.title;
+            $artist.textContent = 'Artist: ' + selTrack.user.username;
+            $artwork.src = selTrack.artwork_url;
+            dwnld(selTrack);
+        });
+    }
+
+    selectedArtist();
+
+})();
